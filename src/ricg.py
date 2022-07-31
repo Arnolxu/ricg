7da496253b422ca0e9ea4dc2a36607b4ff67ea02
Arnolxu <cinaryilmaz.gnu@gmail.com>
2022-07-31 20:34:07 +0300

    Comments

--- src/ricg.py
+++ src/ricg.py
@@ -2,16 +2,21 @@ text = input("Banned characters: ,/~\nEnter a string: ")
 
 out = "=rule "
 
+# space to ~
 words = text.replace(" ", "~")
 
+# make sure the length of the text is a multiple of 4
 if len(words) % 4 != 0:
     words += "~" * (4 - len(words) % 4)
 
+# split the text into two halves
 wp1 = words[:int(len(words)/2)]
 wp2 = words[int(len(words)/2):]
 
+# generate a text that will be parsed by RIC
 i = 0
 for w in range(int(len(wp1) / 2)):
     out += wp1[w * 2] + wp1[w * 2 + 1] + "/" + wp2[w * 2] + wp2[w * 2 + 1] + " "
 
+# print the generated text
 print(out)
